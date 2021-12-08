b7ede599d9429229531d5e4b9df9dfd6e0d4b0b9
markmileTH <73652040+markmileTH@users.noreply.github.com>
2021-12-09 03:23:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <script src="https://code.jquery.com/jquery-3.6.0.js"
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js"
           integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Document</title>
